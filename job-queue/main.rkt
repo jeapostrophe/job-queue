92d0280ce9f431f5d8274657c441968b56d644f7
Jay McCarthy <jay.mccarthy@gmail.com>
2016-11-30 16:48:17 -0500

    simplify

--- job-queue/main.rkt
+++ job-queue/main.rkt
@@ -6,110 +6,72 @@
 
 (define current-worker (make-parameter #f))
 
-(struct job-queue (manager async-channel))
+(struct job-queue (manager-t))
 (struct job (paramz thunk))
-(struct done ())
 
 (define (make-queue how-many)
-  (define jobs-ch (make-async-channel))
   (define work-ch (make-async-channel))
-  (define done-ch (make-async-channel))
-  (define (working-manager spaces accept-new? jobs continues)
-    (if (and (not accept-new?)
-             (empty? jobs)
-             (empty? continues))
-      (first-killing-manager how-many)
-      (apply
-       sync
-       (if (and accept-new?
-                (not (zero? spaces)))
-         (handle-evt
-          jobs-ch
-          (match-lambda
-            [(? job? the-job)
-             (working-manager (sub1 spaces) accept-new? (list* the-job jobs) continues)]
-            [(? done?)
-             (working-manager spaces #f jobs continues)]))
-         never-evt)
-       (handle-evt
-        done-ch
-        (lambda (reply-ch)
-          (working-manager spaces accept-new? jobs (list* reply-ch continues))))
-       (if (empty? jobs)
-         never-evt
-         (handle-evt
-          (async-channel-put-evt work-ch (first jobs))
-          (lambda (_)
-            (working-manager spaces accept-new? (rest jobs) continues))))
-       (map
-        (lambda (reply-ch)
-          (handle-evt
-           (async-channel-put-evt reply-ch
-                                  (if accept-new? 'continue 'stop))
-           (lambda (_)
-             (working-manager (add1 spaces) accept-new? jobs (remq reply-ch continues)))))
-        continues))))
-  (define (first-killing-manager left)
-    (for ([(i quit-sema) (in-hash worker->quit-sema)])
-      (printf "m quit to ~a\n" i)
-      (semaphore-post quit-sema))
-    (killing-manager left))
-  (define (killing-manager left)
-    (unless (zero? left)
-      (sync
-       (handle-evt
-        done-ch
-        (lambda (reply-ch)
-          (printf "m stop at ~a\n" left)
-          (async-channel-put reply-ch 'stop)
-          (killing-manager (sub1 left)))))))
+  (define count-sema (make-semaphore 0))
+  
   (define (worker quit-sema i)
-    (printf "~a waiting for job\n" i)
     (sync
+     ;; Die when this semaphore is signaled
      (handle-evt quit-sema void)
+     ;; Otherwise handle a job
      (handle-evt
       work-ch
       (match-lambda
         [(struct job (paramz thunk))
          (with-handlers ([exn?
                           (λ (e)
-                            (printf "Caught ~v\n" e)
-                            ((error-display-handler) (exn-message e) e)
-                            (printf "Printed\n"))])
+                            ((error-display-handler) (exn-message e) e))])
            (call-with-parameterization
             paramz
             (lambda ()
               (parameterize ([current-worker i])
-                (thunk)))))])))
-    (define reply-ch (make-async-channel))
-    (async-channel-put done-ch reply-ch)
-    (printf "~a waiting for reply\n" i)
-    (define reply-v (async-channel-get reply-ch))
-    (case reply-v
-      [(continue) (worker quit-sema i)]
-      [(stop) (void)]
-      [else
-       (error 'worker "Unknown reply command")]))
+                (thunk)))))
+         ;; Record that the job was successful
+         (semaphore-post count-sema)
+         (worker quit-sema i)]))))
   (define worker->quit-sema
     (for/hasheq ([i (in-range 0 how-many)])
       (define quit-sema (make-semaphore 0))
-      (thread (lambda () (worker quit-sema i)))
-      (values i quit-sema)))
-  (define the-manager
-    (thread (lambda () (working-manager how-many #t empty empty))))
-  (job-queue the-manager jobs-ch))
+      (define worker-t (thread (lambda () (worker quit-sema i))))
+      (values worker-t quit-sema)))
+
+  (define (manager how-many)
+    (match (thread-receive)
+      ['stop
+       (killing-manager how-many)]
+      [(? job? j)
+       (async-channel-put work-ch j)
+       (manager (add1 how-many))]))
+  (define (killing-manager how-many)
+    ;; Wait for all outstanding jobs to finish
+    (for ([i (in-range how-many)])
+      (semaphore-wait count-sema))
+    ;; Tell all workers to die
+    (for ([quit-sema (in-hash-values worker->quit-sema)])
+      (semaphore-post quit-sema))
+    ;; Wait for them to die
+    (for ([worker-t (in-hash-keys worker->quit-sema)])
+      (sync worker-t)))
+  (define manager-t
+    (thread (λ () (manager 0))))
+
+  (job-queue manager-t))
 
 (define (submit-job! jobq thunk)
-  (async-channel-put
-   (job-queue-async-channel jobq)
-   (job (current-parameterization)
-             thunk)))
+  (match-define (job-queue t) jobq)
+  (define j (job (current-parameterization) thunk))
+  ;; XXX If the jobq is in the process of dying, then this will
+  ;; silently throw-away the job
+  (thread-send t j))
 
 (define (stop-job-queue! jobq)
-  (async-channel-put
-   (job-queue-async-channel jobq)
-   (done))
-  (void (sync (job-queue-manager jobq))))
+  (match-define (job-queue t) jobq)
+  (thread-send t 'stop)
+  (void (sync t)))
 
 (provide/contract
  [current-worker (parameter/c (or/c false/c exact-nonnegative-integer?))]
@@ -122,10 +84,13 @@
   (require (submod ".."))
   (define jq (make-job-queue 4))
 
-  (submit-job! jq (λ () (error 'foo)))
-  (submit-job! jq (λ () (error 'bar)))
-  (submit-job! jq (λ () (displayln 4)))
-  (submit-job! jq (λ () (displayln 5)))
+  (for ([i (in-range 100)])
+    (submit-job! jq
+                 (λ ()
+                   ((if (zero? (random 2))
+                      error
+                      displayln)
+                    (number->string i)))))
 
   (stop-job-queue! jq)
 
